c7f3148d6a1406d22937a2f4eb8117a17799bd62
steinerb <briansteiner95@gmail.com>
2020-11-11 20:19:17 -0500

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
 	{ path: 'login', component: LoginComponent },
+	{ path: 'admin', component: AdminComponent },
+	{ path: 'user', component: UserComponent },
 	{ path: '', pathMatch: 'full', redirectTo: 'login' }
 ];
 
